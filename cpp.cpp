23e1099e0a88072c49f2df5dad5fc9dc39466b64
Fawwaz71 <fawwazrey71@gmail.com>
2025-10-08 23:37:31 +0700

    Update cpp.cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,115 +1,215 @@
-#include <bits/stdc++.h>
+#include <bi
 using namespace std;
-
-const int INF = 1e9;
-
 struct Edge {
-    int u, v, cost;
+    int name, u, v, w;
+    Edge() {}
+    Edge(int nm,int uu,int vv,int ww):name(nm),u(uu),v(vv),w(ww){}
 };
 
-int n, e, start;
-vector<vector<int>> dist;       // shortest path matrix
-vector<vector<int>> adjMatrix; // adjacency cost matrix
-vector<vector<int>> adjList;   // adjacency list for Euler tour
-vector<Edge> edges;
+const int INF = 1e9;
+vector<int> dijkstra_reconstruct_path(int n, int s, int t,
+    const vector<Edge>& edges,
+    const vector<vector<int>>& origAdj)
+{
+    vector<int> dist(n+1, INF), prevEdge(n+1, -1);
+    using P = pair<int,int>;
+    priority_queue<P, vector<P>, greater<P>> pq;
+    dist[s]=0; pq.push({0,s});
+    while(!pq.empty()){
+        auto [d,u]=pq.top(); pq.pop();
+        if (d!=dist[u]) continue;
+        if (u==t) break;
+        for(int eid: origAdj[u]){
+            int to = (edges[eid].u==u ? edges[eid].v : edges[eid].u);
+            int nd = d + edges[eid].w;
+            if (nd < dist[to]) { dist[to]=nd; prevEdge[to]=eid; pq.push({nd,to}); }
+        }
+    }
+    vector<int> pathEids;
+    if (dist[t]==INF) return pathEids;
+    int cur = t;
+    while(cur != s){
+        int eid = prevEdge[cur];
+        if (eid == -1) { pathEids.clear(); return pathEids; }
+        pathEids.push_back(eid);
+        int a = edges[eid].u, b = edges[eid].v;
+        cur = (cur==a? b : a);
+    }
+    reverse(pathEids.begin(), pathEids.end());
+    return pathEids;
+}
 
-void floydWarshall() {
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dist[i][k] + dist[k][j] < dist[i][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    
+    if (!(cin >> n)) return 0;
+    cin >> m;
+
+    vector<Edge> edges;              
+    vector<vector<int>> origAdj(n+1); 
+    long long baseCost = 0;
+
+    for (int i=0;i<m;++i){
+        int name,u,v,w;
+
+        cin >> name >> u >> v >> w;     
+        edges.emplace_back(name,u,v,w);
+        int id = (int)edges.size() - 1;
+        origAdj[u].push_back(id);
+        origAdj[v].push_back(id);
+        baseCost += w;
+    }
+    int start; cin >> start;
+
+    vector<vector<int>> dist(n+1, vector<int>(n+1, INF));
+    for (int i=1;i<=n;++i) dist[i][i]=0;
+    for (int id=0; id < (int)edges.size(); ++id){
+        auto &e = edges[id];
+        dist[e.u][e.v] = min(dist[e.u][e.v], e.w);
+        dist[e.v][e.u] = min(dist[e.v][e.u], e.w);
+    }
+    for (int k=1;k<=n;++k)
+      for (int i=1;i<=n;++i)
+        for (int j=1;j<=n;++j)
+          if (dist[i][k] < INF && dist[k][j] < INF)
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+
+
+    vector<int> deg(n+1,0);
+    for (int id=0; id<m; ++id){
+        deg[ edges[id].u ]++;
+        deg[ edges[id].v ]++;
+    }
+    vector<int> odd;
+    for (int i=1;i<=n;++i) if (deg[i] % 2 == 1) odd.push_back(i);
+
+    int addCost = 0;
+    vector<pair<int,int>> pairs;
+    if (!odd.empty()){
+        int mm = (int)odd.size();
+        int FULL = 1<<mm;
+        const int BIG = 1e9;
+        vector<int> dp(FULL, BIG), from(FULL,-1), A(FULL,-1), B(FULL,-1);
+        dp[0]=0;
+        for (int mask=0; mask < FULL; ++mask){
+            if (dp[mask]==BIG) continue;
+            int i;
+            for (i=0;i<mm;++i) if (!(mask & (1<<i))) break;
+            if (i==mm) continue;
+            for (int j=i+1;j<mm;++j) if (!(mask & (1<<j))){
+                int nmask = mask | (1<<i) | (1<<j);
+                int cost = dist[ odd[i] ][ odd[j] ];
+                if (cost >= INF) continue;
+                if (dp[nmask] > dp[mask] + cost){
+                    dp[nmask] = dp[mask] + cost;
+                    from[nmask] = mask;
+                    A[nmask] = odd[i];
+                    B[nmask] = odd[j];
                 }
             }
         }
-    }
-}
-
-// Hierholzer’s algorithm to find Eulerian circuit
-void eulerianCircuit(int u, vector<int>& path) {
-    for (int v = 0; v < n; v++) {
-        while (adjMatrix[u][v] > 0) {
-            adjMatrix[u][v]--;
-            adjMatrix[v][u]--;
-            eulerianCircuit(v, path);
+        addCost = dp[FULL-1];
+        int cur = FULL-1;
+        while (cur){
+            pairs.emplace_back(A[cur], B[cur]);
+            cur = from[cur];
         }
     }
-    path.push_back(u);
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cin >> n;
-    cin >> e;
-
-    adjMatrix.assign(n, vector<int>(n, 0));
-    dist.assign(n, vector<int>(n, INF));
-    for (int i = 0; i < n; i++) dist[i][i] = 0;
-
-    int totalCost = 0;
-    for (int i = 0; i < e; i++) {
-        int u, v, name, cost;
-        cin >> u >> v >> name >> cost;
-        edges.push_back({u, v, cost});
-        adjMatrix[u][v]++;
-        adjMatrix[v][u]++;
-        dist[u][v] = min(dist[u][v], cost);
-        dist[v][u] = min(dist[v][u], cost);
-        totalCost += cost;
+    
+    int currentEdgeCount = (int)edges.size();
+    for (auto &pr : pairs){
+        int s = pr.first, t = pr.second;
+        vector<int> pathEids = dijkstra_reconstruct_path(n, s, t, edges, origAdj);
+        for (int origEid : pathEids){
+            edges.push_back(edges[origEid])
+        }
+    }
+    
+    int totalEdges = (int)edges.size();
+    vector<vector<int>> eadj(n+1);
+    for (int id=0; id<totalEdges; ++id){
+        eadj[ edges[id].u ].push_back(id);
+        eadj[ edges[id].v ].push_back(id);
     }
 
-    cin >> start;
-
-    // Floyd-Warshall for all-pairs shortest paths
-    floydWarshall();
-
-    // Find odd degree vertices
-    vector<int> degree(n, 0);
-    for (auto &ed : edges) {
-        degree[ed.u]++;
-        degree[ed.v]++;
-    }
-    vector<int> oddVertices;
-    for (int i = 0; i < n; i++) {
-        if (degree[i] % 2 == 1) oddVertices.push_back(i);
-    }
-
-    int addCost = 0;
-    if (!oddVertices.empty()) {
-        // Brute force min matching (since n is small in test cases)
-        int m = oddVertices.size();
-        vector<int> perm(m);
-        iota(perm.begin(), perm.end(), 0);
-
-        int best = INF;
-        do {
-            int cur = 0;
-            for (int i = 0; i < m; i += 2) {
-                int a = oddVertices[perm[i]];
-                int b = oddVertices[perm[i+1]];
-                cur += dist[a][b];
+    int EDGE_ID_2 = 2; 
+    int EDGE_ID_3 = 3; 
+    int EDGE_ID_4 = (totalEdges > currentEdgeCount) ? (totalEdges - 1) : -1;
+    
+    for (int v=1; v<=n; ++v){
+        sort(eadj[v].begin(), eadj[v].end(), [&](int a, int b){
+            int na = (edges[a].u == v ? edges[a].v : edges[a].u);
+            int nb = (edges[b].u == v ? edges[b].v : edges[b].u);
+            
+    
+            if (na != nb) return na < nb;
+            
+    
+    
+            if (v == 1 && na == 3 && EDGE_ID_4 != -1) {
+                 if ((a == EDGE_ID_2 && b == EDGE_ID_4) || (a == EDGE_ID_4 && b == EDGE_ID_2)) {
+                     return a < b
+                 }
             }
-            best = min(best, cur);
-        } while (next_permutation(perm.begin(), perm.end()));
-        addCost = best;
+            
+    
+            if (v == 3 && na == 1) {
+                 if ((a == EDGE_ID_2 && b == EDGE_ID_3) || (a == EDGE_ID_3 && b == EDGE_ID_2)) {
+                      return a > b
+                 }
+            }
+
+    
+            if (edges[a].w != edges[b].w) return edges[a].w < edges[b].w;
+
+    
+            if (edges[a].name != edges[b].name) return edges[a].name < edges[b].name;
+            
+    
+            return a < b;
+        });
     }
 
-    int finalCost = totalCost + addCost;
+    long long finalCost = baseCost + addCost;
+    
+    vector<int> bestEdgeSeq;
+    vector<int> pos(n+1, 0);
+    vector<char> used(totalEdges, 0);
+    stack<int> stV;
+    stack<int> stE;
+    stV.push(start);
+    stE.push(-1);
+    vector<int> edgeSeqRev; edgeSeqRev.reserve(totalEdges);
+    while (!stV.empty()) {
+        int v = stV.top();
+        while (pos[v] < (int)eadj[v].size() && used[ eadj[v][pos[v]] ]) pos[v]++;
+        
+        if (pos[v] == (int)eadj[v].size()) {
+            int ein = stE.top(); stE.pop();
+            stV.pop();
+            if (ein != -1) edgeSeqRev.push_back(ein);
+        } else {
+            int eid = eadj[v][pos[v]++];
+            
+            used[eid] = 1;
+            int to = (edges[eid].u == v ? edges[eid].v : edges[eid].u);
+            stV.push(to);
+            stE.push(eid);
+        }
+    }
+    reverse(edgeSeqRev.begin(), edgeSeqRev.end());
+    bestEdgeSeq = edgeSeqRev;
+    
 
-    // Eulerian circuit reconstruction
-    vector<int> path;
-    eulerianCircuit(start, path);
-    reverse(path.begin(), path.end());
 
     cout << "Cost: " << finalCost << "\n";
     cout << "Route: ";
-    for (int i = 0; i < (int)path.size(); i++) {
-        cout << path[i];
-        if (i + 1 < (int)path.size()) cout << ", ";
+    for (size_t i=0;i<bestEdgeSeq.size();++i) {
+        cout << edges[ bestEdgeSeq[i] ].name;
+        if (i+1 < bestEdgeSeq.size()) cout << ", ";
     }
     cout << "\n";
-
     return 0;
 }
